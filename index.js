1526cdfe40da20a8db7c8ad7513d595059019e8e
Wufe <wufe92@gmail.com>
2015-05-21 03:13:12 +0200

    Non resumable support
    Added non resumable support for download

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 var Downloader = require( './lib/downloader.class' );
 var downloader = new Downloader();
-downloader.download( "http://download.thinkbroadband.com/5MB.zip", "5MB2.zip" );
+//downloader.download( "http://download.thinkbroadband.com/5MB.zip", "5MB2.zip" );
+downloader.download( "http://www.xdccfinder.it/2014-06-15_00003.jpg" );
