31dcbb834e7e7b4756e3fad960029c4739af268b
Wufe <wufe92@gmail.com>
2015-05-21 02:50:53 +0200

    Minify commit
    Minified downloader class and deleted chunk class.

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var Downloader = require( './lib/downloader.class' );
 var downloader = new Downloader();
-downloader.download( "http://download.thinkbroadband.com/512MB.zip" );
+downloader.download( "http://download.thinkbroadband.com/5MB.zip", "5MB2.zip" );
